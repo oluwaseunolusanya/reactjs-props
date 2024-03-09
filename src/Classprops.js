1e6118ee0458c012451148868673dc768d08f606
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-03-09 21:04:22 +0000

    Demo on props.

---
+++ src/Classprops.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Classprops extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello {this.props.name} from {this.props.place} !</h1>
+        <p>{this.props.children}</p>
+      </div>
+
+    )
+  }
+}
+
+export default Classprops
---
+++ src/Functionprops.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Functionprops(props) {
+  return (
+    <div>
+            <h3>This is a functional component {props.name}.</h3>
+    </div>
+  );
+}
+
+export default Functionprops
